884855cccdc8b04bc4a357396cf3146ee12f7774
Mohd Zaid <[razvizaid259@gmail.com]>
2024-05-25 11:26:50 +0530

    github added

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const gitData={
+  'login':'mohdzaid',
+  'id':193234,
+  'address':'india'
+}
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
@@ -11,6 +17,9 @@ app.get('/login',(req,res)=>{
   res.send('<h1>login at chat or code</h1>')
 })
 
+app.get('/github',(req,res)=>{
+  res.json(gitData)
+})
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${process.env.PORT}`)
 })
